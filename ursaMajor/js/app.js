94f2e5b2c36fd70670edb0a9ed16c9786ce03a77
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-04-17 22:29:55 -0700

    Added basic pile representation

--- ursaMajor/js/app.js
+++ ursaMajor/js/app.js
@@ -1,5 +1,22 @@
 var ursaMajor = angular.module('ursaMajor', []);
 
+ursaMajor.factory('requirementsFactory', function() {
+
+	var factory = {};
+	factory.getRequirements = function() {
+		return 1;
+	}
+
+})
+
 ursaMajor.controller('PileController', function($scope) {
+
+	$scope.displayedCourses = [];
+
+})
+
+ursaMajor.controller('PathController', function($scope, requirementsFactory) {
+
+	$scope.requirements = [];
 
 })
