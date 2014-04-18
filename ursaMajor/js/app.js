8f5acd868535cb08e12afb6c9d628a6b2f720e46
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-04-17 23:08:51 -0700

    Added course search to pile

--- ursaMajor/js/app.js
+++ ursaMajor/js/app.js
@@ -4,14 +4,37 @@ ursaMajor.factory('requirementsFactory', function() {
 
 	var factory = {};
 	factory.getRequirements = function() {
-		return 1;
+		// TODO: get requirements of a particular major as JSON
+		return null;
 	}
+
+	return factory;
+})
+
+ursaMajor.factory('coursesFactory', function() {
+
+	var factory;
+	factory.getCourses = function(string) {
+		// TODO: return a list of courses that contain the given search string
+		return null;
+	}
+
+	return factory;
+})
+
+ursaMajor.factory('userCourses', function() {
 
 })
 
-ursaMajor.controller('PileController', function($scope) {
+ursaMajor.controller('PileController', function($scope, coursesFactory) {
 
 	$scope.displayedCourses = [];
+
+	$scope.searchForCourse = function() {
+		setTimeout(function() {
+			$scope.displayedCourses = coursesFactory.getCourses($scope.courseSearch);
+		}, 200);
+	}
 
 })
 
